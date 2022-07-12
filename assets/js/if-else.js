03f167757b0bbb6663d2eb641f845dd76eac0106
Christian Alejandro Ramos Pérez <94751604+Chrisburg22@users.noreply.github.com>
2022-07-12 10:28:23 -0500

    Estructuras de control
    Estructuras de conrtol
    Logico Booleana

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -20,3 +20,22 @@ if ( dia === 0 ){
 } else if ( dia === 2 ){
     console.log(`Hoy es martes`);
 }
+
+//PODEMOS HACER CON MISMO QUE CON EL IF-ELSE CON ARREGLOS O UN OBJETO. ESTA ES UNA VENTAJA YA QUE NO LOS NECESITAMOS TODO EL TIEMPO SABIENDO UTILIZAR LOS RECURSOS NECESARIOS
+dia = 2;
+//Objeto
+const diaLetras = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miercoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sabado',
+}
+
+console.log( diaLetras[dia] || 'Dia no defindo');
+
+//Array
+const diaLetras2 = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+console.log( diaLetras2[ dia ]);
---
+++ assets/js/logica-booleana.js
@@ -0,0 +1,61 @@
+const regresaTrue = ( ) => {
+    console.log('Regresa True');
+    return true;
+}
+
+const regresaFalse = ( ) => {
+    console.log('Regresa False');
+    return false;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+console.warn(`El operador booleano untilizado es el de negación " ! "`);
+console.log( true );
+console.log( !true );
+console.log( !false );
+console.log( !regresaFalse() );
+
+console.warn(`El operador utilizado es el And " && "`);
+console.log( true && true );
+console.log( true && !false);
+console.log( false && true );
+
+console.log('=====================================');
+console.log( regresaFalse() && regresaTrue() );//Cuando en primer parametro es false el operador 'AND' entiende que ya no es necesario correr toda esa linea de código por que ya sabe que el retorno el falso
+console.log( regresaTrue && regresaFalse() );
+
+console.log('=====================================');
+regresaFalse() && regresaTrue();
+regresaTrue() && regresaFalse();
+
+console.log( `4 Operadores And`, true && true && true && false );
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+console.warn(`El operador utilizado es el OR " | | "`);
+console.log( true || true );
+console.log( false || false );
+
+console.log( regresaTrue() || regresaFalse() );
+console.log( `4 Operadores Or`, true || true || true || false );
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ console.warn('Asignaciones con booleanos');
+
+ const soyUndefined = undefined;
+ const soyFalse = false;
+ const soyNull = null;
+
+ const a1 = false && 'Hola mundo' && 150; //150
+ const a2 = 'Hola' && 'Mundo' && true && false;//false
+ const a3 = soyFalse || 'Ya no soy falso'; //Ya no soy falso
+ const a4 = soyFalse || soyNull || soyUndefined || 'Ya no soy null, ni false y menos undefined' || true;
+ const a5 = soyFalse || soyNull || regresaFalse() || regresaTrue || 'Ya nos soy false nunca más';
+
+ console.log( {a1, a2, a3, a4, a5} );
+
+ //Ejempo de como es utilizada los operadores booleanos para estructuras de control
+ if( regresaFalse() && regresaTrue() && ( true || false || true ) ){
+    console.log('Hacer algo');
+ } else {
+    console.log('Hacer otra cosa');
+ }
